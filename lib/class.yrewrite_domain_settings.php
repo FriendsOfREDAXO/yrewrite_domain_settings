c1c48e5a9c2a86c23c36dd5fa4749bd13748119d
Thomas Skerbis <thomas.skerbis@mac.com>
2020-06-19 17:23:00 +0200

    Update class.yrewrite_domain_settings.php

--- lib/class.yrewrite_domain_settings.php
+++ lib/class.yrewrite_domain_settings.php
@@ -1,149 +1,39 @@
-<?php
-
-/**
- * This file is part of the plyr package.
- *
- * @author (c) Friends Of REDAXO
- * @author <friendsof@redaxo.org>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-class rex_plyr
+<?php class yrewrite_domain_settings
 {
 
-    public static function checkUrl($url)
+
+    public function __construct()
     {
-        if ($url) {
-            if (filter_var($url, FILTER_VALIDATE_URL) === false) {
-            }
-            if (file_exists(rex_path::media($url)) === true) {
-                $url = rex_url::media($url);
-                return $url;
-            }
-            return $url;
-        }
+        $this->addon = rex_addon::get(rex::getTablePrefix().'yrewrite_domain_settings');
+        $this->domain = rex_yrewrite::getCurrentDomain();
     }
 
-    public static function checkYoutube($url)
-    {
-        if (preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $url)) {
-            return true;
-        }
-        return false;
-    }
-
-    public static function getYoutubeId($urL)
-    {
-        $youtubeID = "";
-        if (preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $urL, $match)) {
-            $youtubeID = $match[1];
-        }
-        return $youtubeID;
-    }
-
-    public static function checkMedia($url)
-    {
-        $media = rex_media::get($url);
-        $checkPath = pathinfo($url);
-        if ($media) {
-            if (strtolower($checkPath['extension']) == "mp4") {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static function checkVideo($url)
-    {
-        if (rex_plyr::checkYoutube($url) || rex_plyr::checkVimeo($url) || rex_plyr::checkMedia($url)) {
-            return true;
-        }
-        return false;
-    }
-
-
-
-
-
-    public static function checkAudio($url)
-    {
-        $audio = rex_media::get($url);
-        $checkPath = pathinfo($url);
-        if ($audio) {
-            if (strtolower($checkPath['extension']) == "mp3") {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static function checkVimeo($url)
-    {
-        if (preg_match('~(?:<iframe [^>]*src=")?(?:https?:\/\/(?:[\w]+\.)*vimeo\.com(?:[\/\w]*\/videos?)?\/([0-9]+)[^\s]*)"?(?:[^>]*></iframe>)?(?:<p>.*</p>)?~ix', $url)) {
-            return true;
-        }
-        return false;
-    }
-
-    public static function getVimeoId($url)
-    {
-        $vimeoID = "";
-        if (preg_match('~(?:<iframe [^>]*src=")?(?:https?:\/\/(?:[\w]+\.)*vimeo\.com(?:[\/\w]*\/videos?)?\/([0-9]+)[^\s]*)"?(?:[^>]*></iframe>)?(?:<p>.*</p>)?~ix', $url, $match)) {
-            $vimeoID = $match[1];
-        }
-        return $vimeoID;
-    }
-
-    public static function checkExternalMp4($url)
-    {
-        $checkSource = get_headers($url, 1);
-        if (isset($checkSource['Content-Type']) && $checkSource['Content-Type'] === 'video/mp4') {
-            return true;
-        }
-        return false;
-    }
-
-    public static function outputMedia($url, $controls = NULL, $poster = NULL)
+    public static function getValue($sKey = null)
     {
 
-        $player = new rex_plyr();
-        $link = $player->checkUrl($url);
+        $oSettings = new yrewrite_domain_settings;
 
-        if ($controls) {
-            $player_conf = json_encode(explode(",", $controls));
-            $controls = ' data-plyr-config=\'{"controls":' . $player_conf . '}\'';
+        $iDomainsId = $oSettings->domain->getId();
+
+        if (!$iDomainsId) {
+            return;
         }
 
-        if ($player->checkYoutube($link) == true) {
-            $out = '<div class="rex-plyr" data-plyr-provider="youtube" data-plyr-embed-id="' . $player->getYoutubeId($link) . '"' . $controls . '></div>';
-        }
-        if ($player->checkVimeo($link) == true) {
-            $out = '<div class="rex-plyr" data-plyr-provider="vimeo" data-plyr-embed-id="' . $player->getVimeoId($link) . '"' . $controls . '></div>';
-        }
-        if ($player->checkMedia($url) !== false || $player->checkExternalMp4($url) === true) {
-
-            if ($poster) {
-                $poster = ' poster="' . $poster . '"';
-            }
-
-            $out = '
-                        <video class="rex-plyr"' . $controls . ' playsinline volume=1' . $poster . '>
-                            <source src="' . $link . '" type="video/mp4">
-                        </video>
-                    ';
+        $oQuery = rex_yform_manager_table::get(rex::getTablePrefix().'yrewrite_domain_settings')->query();
+        $oQuery->where('domain_id', $iDomainsId, '=');
+        $oItem = $oQuery->findOne();
+        if (!count($oItem)) {
+            return;
         }
 
-        if ($player->checkAudio($url) !== false) {
-            $out = '
-                        <audio class="rex-plyr"' . $controls . '>
-                            <source src="' . $link . '" type="audio/mp3">
-                        </audio>
-                    ';
+        if (!$oItem->getValue('id')) {
+            return;
         }
 
-        return $out;
+        if ($sKey != "") {
+            return $oItem->getValue($sKey);
+        } else {
+            return $oItem->getData();
+        }
     }
 }
-
